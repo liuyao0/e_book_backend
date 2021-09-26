8934805ef456d91388f944939236893354412f01
liuyao <liu.yao@sjtu.edu.cn>
2021-09-27 01:27:20 +0800

    eqw

---
+++ src/main/java/com/ebook/ebook/webfilter/MyFilter.java
@@ -0,0 +1,36 @@
+package com.ebook.ebook.webfilter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class MyFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletRequest httpServletRequest=(HttpServletRequest) request;
+        httpServletResponse.setHeader(
+                "Baeldung-Example-Filter-Header", "Value-Filter");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin",httpServletRequest.getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,POST");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers","*");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials","true");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
